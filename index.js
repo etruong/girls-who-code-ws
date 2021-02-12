28418fd62b2d55d72c4b00e0c8d716d346e93edc
Elisa <elisat15@uw.edu>
2021-02-11 21:25:50 -0800

    add instructions

---
+++ index.js
@@ -0,0 +1,18 @@
+/***************************/
+/* Girls Who Code Workshop */
+/***************************/
+
+/* Change Message of Note functionality */
+
+/* Change font color functionality */
+
+/* Change sender name on note */
+
+/* Dynamic generate color options */
+
+
+// Extra time? //
+
+/* Change image functionality */
+
+/* Change reciever name on note */
